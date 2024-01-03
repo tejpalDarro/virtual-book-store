90c6c2b0ab747e380b88117c43ceac347ec1123d
Tejpal Darro <tejpaldarro007@gmail.com>
2024-01-03 23:35:53 +0530

    initial file

---
+++ src/test/java/com/phenol/virtualbookstore/VirtualBookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phenol.virtualbookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualBookStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
